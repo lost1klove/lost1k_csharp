89495f61809efdeb2de8721e3407917f0ac92909
lost1k <only.lost1k@gmail.com>
2022-10-11 23:46:18 +0300

    add ex05 ifElse

---
+++ example05_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "andrei")
+{
+    Console.WriteLine("Ура, это же Андрей!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
