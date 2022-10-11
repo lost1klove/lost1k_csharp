a7e31ca51fabcca3c26c890bf93a6db4bfb22a06
lost1k <only.lost1k@gmail.com>
2022-10-12 01:08:45 +0300

    add HOMEWORK

---
+++ HW_example8_dog/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int count = 0, distance = 10000, firstFriendSpeed = 1, SecondFriendSpeed = 2, dogSpeed = 5, time = 0, friend = 2;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (SecondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    if (friend == 2)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+
+    distance = distance - ((firstFriendSpeed + SecondFriendSpeed) * time);
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз");
+
