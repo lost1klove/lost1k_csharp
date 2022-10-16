f332899d51721f71848ec210288821352bf59a59
lost1k <only.lost1k@gmail.com>
2022-10-17 00:30:41 +0300

    add ex09&ex10

---
+++ example09_introMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+
+int a1 = 3;
+int a2 = 5;
+int a3 = 7;
+int b1 = 13;
+int b2 = 23;
+int b3 = 31233;
+int c1 = 12;
+int c2 = 15;
+int c3 = 345;
+
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3)
+    );
+
+
+
+Console.WriteLine(max);
