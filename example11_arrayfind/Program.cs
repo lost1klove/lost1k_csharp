bd4f46c43835b511a07a2dcdaa6c4f4205f219d7
lost1k <only.lost1k@gmail.com>
2022-10-17 01:08:35 +0300

    added ex11 and ex12

---
+++ example11_arrayfind/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 21, 13,18, 4, 51, 61, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
