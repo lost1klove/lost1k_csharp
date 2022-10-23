acf9f7280ba60d3088e8d2eaeb78b2aea2fff484
lost1k <only.lost1k@gmail.com>
2022-10-24 01:59:29 +0300

    add for and edit methods

--- examplemethods/Program.cs
+++ examplemethods/Program.cs
@@ -71,3 +71,15 @@ string Method4(int count, string text )
 }
 string res = Method4(10, "GB ");
 System.Console.WriteLine(res);
+
+
+
+
+for(int i = 2; i<=10;i++)
+{
+    for(int j = 2; j<=10;j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine("");
+}
